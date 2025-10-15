25f0f8782617683f0fc6856ea852798a32f6569c
Gabriel Badillo <gbadillocolberg@gmail.com>
2025-10-14 21:52:46 -0400

    log

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const db = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
-console.log(process.env);
+console.log(here);
 
 app.get('/', (re, res) => {
     return res.json("From Backend Side");
